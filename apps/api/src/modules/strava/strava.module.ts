cf54080fefd1fc82f1bb3344ed7a451eae89b41d
frontov <roman@frontov.ru>
2026-04-10 14:18:35 +0300

    Fix Strava module JWT dependency

--- apps/api/src/modules/strava/strava.module.ts
+++ apps/api/src/modules/strava/strava.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 
 import { StravaController } from "./strava.controller";
 import { StravaProcessor } from "./strava.processor";
 import { StravaService } from "./strava.service";
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [StravaController],
   providers: [StravaService, StravaProcessor],
   exports: [StravaService],
